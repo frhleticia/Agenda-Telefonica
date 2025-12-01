25d536cb16add265ec2d8f07bf6ccc0595acdbc2
Le <frhleticia@gmail.com>
2025-12-01 10:32:11 -0300

    Refactor Main class to manage multiple contacts

--- Main.java
+++ Main.java
@@ -1,8 +1,20 @@
+package POO.AgendaTelefonica;
+
 public class Main {
     public static void main(String[] args) {
-    Contato irmao = new Contato("Lucas", 960511773);
+    Contato irmao = new Contato("Edu", "(22)99988-8777");
+    Contato irma = new Contato("Mônica", "(51)98765-4321");
+    Contato leticia = new Contato("Letícia", "(51)99333-1993");
 
-    AgendaTelefonica daLeticia = new AgendaTelefonica(irmao);
-    daLeticia.mostrarAgenda();
-    }
+    Agenda agendaDaLe = new Agenda(leticia);
+
+    agendaDaLe.adicionarContato(irmao);
+    agendaDaLe.mostrarAgenda();
+
+    agendaDaLe.adicionarContato(irma);
+    agendaDaLe.mostrarAgenda();
+
+    agendaDaLe.removerContato(irmao);
+    agendaDaLe.mostrarAgenda();
+   }
 }
