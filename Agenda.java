0ded41cad77fc4603d0afd99c3529d006103ac9a
Le <frhleticia@gmail.com>
2025-12-10 01:46:00 -0300

    Refactor Agenda class structure and formatting

--- Agenda.java
+++ Agenda.java
@@ -3,23 +3,20 @@ import java.util.ArrayList;
 
 public class Agenda {
     private Contato contato;
+    private ArrayList<Contato> agenda = new ArrayList<>();
 
     public Agenda(Contato contato){
         this.contato = contato;
     }
 
-    ArrayList<Contato> agenda = new ArrayList<>();
-
     public void mostrarAgenda(){
         System.out.println("˗ˋˏ°•୨ Agenda de "+this.contato.getNome()+" ୧•°ˎˊ˗");
         System.out.println("Meu número: " + this.contato.getNumero());
         for (Contato contato : agenda){
-
             System.out.println("Nome: " + contato.getNome() + "\nTelefone: " + contato.getNumero());
         }
         System.out.println("\n");
     }
-
     public void adicionarContato(Contato contato){
         agenda.add(contato);
     }
